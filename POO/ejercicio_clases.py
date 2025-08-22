848344c115ffa0dd23fb3ecb0ae142b2422bef0f
KeylerMisath <keylermisathm07@gmail.com>
2025-08-21 21:09:15 -0500

    Update ejercicio_clases.py

--- POO/ejercicio_clases.py
+++ POO/ejercicio_clases.py
@@ -4,20 +4,39 @@ class Biblioteca:
         self.autor = autor
         self.estado = estado
 
-    def Registro(self):
-        self.titulo = str(input("¿Qué libro desea registrar? "))
-        self.autor = str(input("¿Qué autor tien el libro? "))
-        self.estado = str(input("¿En qué estado se encuentra su libro?: "))
-        return print(f"Has registrado: {self.titulo} de {self.autor}.")
-    
-    def status(self, disponible, prestado):
-        self.disponible = "disponible"
-        self.prestado = "prestado"
-        if self.tiulo == "prestado":
-            print(f"El libro {self.titulo} se encuentra prestado")
+    def registrar(self):
+        self.titulo = input("¿Qué libro desea registrar?: ")
+        self.autor = input("¿Quién es el autor del libro?: ")
+        print(f"Has registrado: {self.titulo} de {self.autor}.")
 
-        elif self.titulo == "Disponible":
-            print(f"El libro {self.titulo} se encuentra disponible")    
+    def prestar(self):
+        self.estado = input("¿En qué estado se encuentra su libro? (disponible/prestado): ")
 
-Libro1 = Biblioteca(False, False, False)
-Libro1.Registro() 
+        if self.estado == "disponible":
+            self.estado = "prestado"
+            print(f"El libro '{self.titulo}' se encuentra disponible.")
+
+        elif self.estado == "prestado":
+            print(f"El libro '{self.titulo}' ya está prestado.")
+
+
+    def devolver(self):
+        if self.estado == "prestado":
+            status = input("Va a devolver un libro? (si/no): ")
+            if status.lower() == "si":
+                libro_devuelto = input("¿Qué libro va a devolver?: ")
+                if libro_devuelto == self.titulo:
+                    self.estado = "disponible"
+                    print(f"El libro '{libro_devuelto}' ha sido devuelto y está disponible.")
+                else:
+                    print(f"El libro '{libro_devuelto}' no coincide con el que está prestado.")  # aquí es en caso de q no se cumpla esa condición 
+            elif status.lower() == "no":
+                    print("No se realizó ninguna devolución.") 
+        else:
+            pass
+
+
+libro = Biblioteca("", "", "disponible")
+libro.registrar()
+libro.prestar()
+libro.devolver()
